07e338a18ae1d175a84e66219c45f02ca8f8c425
mario <mario@mario>
2018-03-17 18:47:50 +0100

    Clase Sudoku_ed terminada

--- src/sudokumario_ed/Sudoku_ed.java
+++ src/sudokumario_ed/Sudoku_ed.java
@@ -36,26 +36,79 @@ public class Sudoku_ed {
         aleatorio = new Random();
     }
 
+    /**
+     * Metodo toString muestra el resulado final     *
+     * @return el resultado
+     */
     @Override
     public String toString() {
         String resultadoFinal = "";
+        for (int i = 0; i < numeros.size(); i++) {
+
+            for (int b = 0; b < 9; b++) {
+
+                int n = numeros.get(i).get(b);
+                resultadoFinal += n + " ";
+            }
+            resultadoFinal += "\n";
+
+        }
 
         return resultadoFinal;
     }
 
-    public void modificarElemento(int fila, int columna, int elemento) throws
-            SudokuException {
+    /**
+     * modifica un elemento del tablero por el que le pasemos nosotros en la
+     * posición que nosotros le pasemos
+     *
+     * @param fila donde queremos insertar el elemento
+     * @param columna donde queremos insertar el elemento
+     * @param elemento número que queremos insertar en el tablero
+     * @throws SudokuException
+     */
+    
+    
+    public void modificarElemento(int fila, int columna, int elemento) throws SudokuException {
+        if (puedoInsertar(fila, columna, elemento) == true) {
+            numeros.get(fila - 1).set(columna - 1, elemento);
+        } else {
+            System.out.println("No se puede insertar el número.");
+        }
 
     }
 
+    /**
+     * Pone un elemento vacío
+     *
+     *
+     * @param fila donde está el elemento que queremos que este vacío
+     * @param columna donde está el elemento que queremos que este vacío
+     */
+    
+    
     public void vaciarElemento(int fila, int columna) {
-
+        numeros.get(fila - 1).set(columna - 1, 0);
     }
 
+    /**
+     * Comprueba que él elemento que introducimos no esté repetido en la misma
+     * fila
+     *
+     *
+     * @param fila fila donde introducimos el elemento
+     * @param elemento elemento que queremos introducir
+     * @return true si se puede introducir y false si no se puede introdudcir
+     */
+    
+    
     private boolean comprobarFila(int fila, int elemento) {
-        boolean resultado = true;
-
-        return resultado;
+        boolean error = true;
+        for (int i = 0; i < numeros.size(); i++) {
+            if (numeros.get(fila - 1).get(i) == elemento) {
+                error = false;
+            }
+        }
+        return error;
     }
 
     /**
@@ -65,11 +118,13 @@ public class Sudoku_ed {
      * @param elemento numero que añadimos
      * @return true o false depende de si no aparece el numero en la columna
      */
+    
+    
     private boolean comprobarColumna(int columna, int elemento) {
 
         boolean resultado = true;
         for (int i = 0; i < numeros.size(); i++) {
-            for (int j = 0; j < numeros.size(); j++) {
+            for (ArrayList<Integer> numero : numeros) {
                 if (numeros.get(i).get(columna - 1) == elemento) {
                     resultado = false;
                 }
@@ -78,15 +133,75 @@ public class Sudoku_ed {
         return resultado;
     }
 
+    /**
+     * Comprueba el numero(elemento) en el cuadrante
+     *
+     * @param fila recive la fina del cuadrante
+     * @param columna recive la columna del cuadrante
+     * @param elemento numero que vamos a insertar
+     * @return sepuede si es true se puede insertar el elemento
+     */
+    
+    
     private boolean comprobarCuadrante(int fila, int columna, int elemento) {
-        boolean resultado = true;
+        boolean sepuede = true;
+        int v_x = 0, v_y = 0, terminax = 0, terminay = 0;
+        int n_cuadrante = 0;
+        if (fila >= 0 && fila <= 2) {
+            v_x = 0;
+            terminax = 2;
+        } else if (fila >= 3 && fila <= 5) {
+            v_x = 3;
+            terminax = 5;
+        } else if (fila >= 6 && fila <= 8) {
+            v_x = 6;
+            terminax = 8;
+        }
+        if (columna >= 0 && columna <= 2) {
+            v_y = 0;
+            terminay = 2;
+        } else if (columna >= 3 && columna <= 5) {
+            v_y = 3;
+            terminay = 5;
+        } else if (columna >= 6 && columna <= 8) {
+            v_y = 6;
+            terminay = 8;
+        }
 
-        return resultado;
+        for (int i = v_x; i <= terminax; i++) {
+            for (int j = v_y; j <= terminay; j++) {
+                if (numeros.get(i).get(j) == elemento) {
+
+                    sepuede = false;
+
+                }
+
+            }
+        }
+
+        return sepuede;
     }
 
+    /**
+     * Este metodo se encarga de comprobar todos nuestros metodos anteriores
+     * para ver si se puede colocar el numero
+     *
+     *
+     * @param fila donde se encuentra el elemento
+     * @param columna donde se encuentra el elemento
+     * @param elemento que queremos insertar
+     * @return true si se puede insertar y false si no se puede insertar
+     */
+    
+    
     private boolean puedoInsertar(int fila, int columna, int elemento) {
-        boolean resultado = false;
+        boolean ressult = false;
 
-        return resultado;
+        if (comprobarFila(fila, elemento) == true && comprobarColumna(columna, elemento) == true && comprobarCuadrante(fila, columna, elemento) == true) {
+
+            ressult = true;
+
+        }
+        return ressult;
     }
 }
